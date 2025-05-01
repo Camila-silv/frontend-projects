915c13797b58b43faa169a5eb82552ca30236e26
Camila Silva <cf.silv466@gmail.com>
2025-04-30 22:22:30 -0300

    [FEAT] main content

--- dev-hub/src/js/main.js
+++ dev-hub/src/js/main.js
@@ -1,5 +1,17 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+
+const modalFocus = document.querySelector("[data-modal]");
+const inputPost = document.querySelector("[data-post]");
+
+modalFocus.addEventListener("click", () => {
+  setTimeout(() => {
+    inputPost.focus();
+  }, 500);
+});
+
+
+
